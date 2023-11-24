312d650b0f56d6eee8fb360278b12c598deadd3c
suparnMarathe <suparnm98@gmail.com>
2023-11-24 16:29:37 +0530

    outputting a calculation table is done with new component

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,10 +1,34 @@
-import { calculateInvestmentResults } from "../util/investment";
-
+import { calculateInvestmentResults,formatter } from "../util/investment";
 
 export default function Results({ input }) {
-    const resultData= calculateInvestmentResults(input);
-    console.log(resultData);
-
-  return <></>;
-
+  const resultData = calculateInvestmentResults(input);
+  const initialInvestment=resultData[0].valueEndOfYear-(resultData[0].annualInvestment+resultData[0].interest);
+  return (
+    <table id="result">
+      <thead>
+        <tr>
+          <th>Year</th>
+          <th>Investment Value</th>
+          <th>Interest (Year)</th>
+          <th>Total Interest</th>
+          <th>Invested Capital</th>
+        </tr>
+      </thead>
+      <tbody>
+        {resultData.map((yearData) => {
+            let totalInterest=yearData.valueEndOfYear-initialInvestment-(yearData.year*yearData.annualInvestment);
+            let investedCapital=yearData.valueEndOfYear-totalInterest;
+          return (
+            <tr key={yearData.year}>
+              <td>{yearData.year}</td>
+              <td>{formatter.format(yearData.valueEndOfYear)}</td>
+              <td>{formatter.format(yearData.interest)}</td>
+              <td>{formatter.format(totalInterest)}</td>
+              <td>{formatter.format(investedCapital)}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
 }
